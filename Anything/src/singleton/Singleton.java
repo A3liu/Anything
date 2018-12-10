1caebd9e530663a717589ad21e132d532ce4c4b6
liuyaosen <275383374@qq.com>
2018-12-10 20:19:26 +0800

    双重检查锁单例

---
+++ Anything/src/singleton/Singleton.java
@@ -0,0 +1,21 @@
+package singleton;
+
+
+public class Singleton {
+    private static Singleton unique;
+
+    private Singleton() {
+    }
+
+    public Singleton getInstance() {
+        if (unique == null) {
+            synchronized (Singleton.class) {
+                if (unique == null) {
+                    unique = new Singleton();
+                }
+            }
+        }
+        return unique;
+    }
+
+}
