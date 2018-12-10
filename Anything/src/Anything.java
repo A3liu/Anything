5d68aa59e1e30dd78c78a6b4041ddb92dbe17304
liuyaosen <275383374@qq.com>
2018-12-10 20:31:37 +0800

    dddd

--- Anything/src/Anything.java
+++ Anything/src/Anything.java
@@ -1,12 +1,10 @@
-import java.io.UnsupportedEncodingException;
-import java.util.Arrays;
-
-public class Anything { 
-	public static void main(String[] args) throws UnsupportedEncodingException {
+public class Anything {
+    public static void main(String[] args) {
 		String a="asdfasf.jpg";
 		System.out.println(a.matches("^.*\\.(jpg|bmp|png)$"));
 		int b=8;
 		int c=b << 1;
 		System.out.println(c);
+        System.out.println("dddd");
 	}
 }
